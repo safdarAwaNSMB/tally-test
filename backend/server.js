d7d69ad51d12512f8af7f62f4aac8896e25006b6
safdarAwaNSMB <safdar.awan@weeshare.com>
2024-05-02 10:57:23 +0500

    Authorization

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ app.get("/twitter-success/:code", async (req, res) => {
   try {
     const { code } = req.params;
     const grantType = "authorization_code";
-    const clientId = "V1FrUFdVZ3picVFSUGtHWExpR1I6MTpjaQ"; // Replace with your client ID
+    const clientId = "enZoZHJ3cDlxbWtfYXRlTXJYS0M6MTpjaQ"; // Replace with your client ID
     const redirectUri = "http://localhost:5173/twitter-success"; // Replace with your redirect URI
     const codeVerifier = "abc123ABC"; // Replace with your code verifier (should be the same as used during authorization)
 
--- frontend/src/components/SingleQuest.vue
+++ frontend/src/components/SingleQuest.vue
@@ -44,7 +44,7 @@ async function connectTwitter() {
     try {
         const csrfState = Math.random().toString(36).substring(2);
 
-        let url = `https://twitter.com/i/oauth2/authorize?response_type=code&client_id=V1FrUFdVZ3picVFSUGtHWExpR1I6MTpjaQ&redirect_uri=http://localhost:5173/twitter-success&scope=tweet.read users.read offline.access&state=${state}&code_challenge=abc123ABC&code_challenge_method=plain`;
+        let url = `https://twitter.com/i/oauth2/authorize?response_type=code&client_id=enZoZHJ3cDlxbWtfYXRlTXJYS0M6MTpjaQQ&redirect_uri=http://localhost:5173/twitter-success&scope=tweet.read users.read offline.access&state=${state}&code_challenge=abc123ABC&code_challenge_method=plain`;
         url += "&state=" + csrfState;
         window.open(url, '_blank');
     } catch (error) {
