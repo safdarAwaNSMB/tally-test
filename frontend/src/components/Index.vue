cbb057d6da9368319435b50e9326cb4b5a73f3ca
safdarAwaNSMB <safdar.awan@weeshare.com>
2024-05-10 21:44:13 +0500

    updated required attributes

--- frontend/src/components/Index.vue
+++ frontend/src/components/Index.vue
@@ -1,40 +1,44 @@
 <script setup>
-import { onMounted, ref } from 'vue';
-import Quest from './Quest.vue'
-import axios from 'axios';
+import { onMounted, ref } from "vue";
+import Quest from "./Quest.vue";
+import axios from "axios";
 const backendUrl = import.meta.env.VITE_APP_BACKEND_URL;
 const quests = ref(null);
 const loading = ref(true);
 
-
 onMounted(async () => {
-    loading.value = true;
-    await axios.get(`${backendUrl}/get-all-quests`).then(res => {
-        console.log(res);
-        quests.value = res.data.data
-    }).catch(err => {
-        console.log(err);
-    }).finally(() => {
-        loading.value = false
+  loading.value = true;
+  await axios
+    .get(`${backendUrl}/get-all-quests`)
+    .then((res) => {
+      console.log(res);
+      quests.value = res.data.data;
     })
+    .catch((err) => {
+      console.log(err);
+    })
+    .finally(() => {
+      loading.value = false;
+    });
 });
 </script>
 
 <template>
-    <section class="flex md:w-2/3 w-full mx-auto m-4 p-5 flex-col justify-center">
-        
-            
-                <h3 class="text-center text-white text-3xl font-bold my-3">Available Quests</h3>
-                <div v-if="loading" class="flex mt-5 justify-center items-center w-full ">
-                    <div  class="inline-block me-3 h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-white motion-reduce:animate-[spin_1.5s_linear_infinite]"
-                    role="status">
-                    <span
-                    class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
-                    >Loading...</span
-                    >
-                </div>
-            </div>
-                <Quest v-for="(quest, index) in quests" :key="index" :data="quest" />
-           
-    </section>
+  <section class="flex md:w-2/3 w-full mx-auto m-4 p-5 flex-col justify-center">
+    <h3 class="text-center text-white text-3xl font-bold my-3">
+      Available Quests
+    </h3>
+    <div v-if="loading" class="flex mt-5 justify-center items-center w-full">
+      <div
+        class="inline-block me-3 h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-white motion-reduce:animate-[spin_1.5s_linear_infinite]"
+        role="status"
+      >
+        <span
+          class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
+          >Loading...</span
+        >
+      </div>
+    </div>
+    <Quest v-for="(quest, index) in quests" :key="index" :data="quest" />
+  </section>
 </template>
--- frontend/src/components/Quest.vue
+++ frontend/src/components/Quest.vue
@@ -1,5 +1,6 @@
 <script setup>
 import rightArrow from "../assets/chevron-right-solid.svg";
+import questLogo from "../assets/images/questLogo.png";
 const backendUrl = import.meta.env.VITE_APP_BACKEND_URL;
 const props = defineProps(['data']);
 console.log(props.data);
@@ -10,7 +11,7 @@ console.log(props.data);
     <a :href="`http://localhost:5173/quest/${props.data?._id}`">
         <div class="flex my-2 flex-row w-full items-center bg-sky-400 justify-between rounded-lg md:px-8 p-4">
             <div class="flex flex-row items-center gap-2">
-                <img  class="rounded-full w-12 h-12" :src="backendUrl + '/' + props.data?.questImage" alt="image">
+                <img  class="rounded-full w-12 h-12" :src="(backendUrl + '/' + props.data?.questImage) || questLogo" alt="image">
                 <h1 class="text-xl">{{ props.data?.questName }}</h1>
             </div>
             <img :src="rightArrow" class="w-6" />
