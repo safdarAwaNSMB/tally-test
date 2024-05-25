496d577ad1e7fec85ae622820e0d0fed53199dc2
safdarAwaNSMB <safdar.awan@weeshare.com>
2024-05-25 12:30:44 +0500

    url set

--- frontend/src/views/TwitterSuccess.vue
+++ frontend/src/views/TwitterSuccess.vue
@@ -9,39 +9,39 @@ const route = useRoute();
 const code = ref(null);
 
 
-watch(code, async () => {
-    if (code) {
-        const response = await axios.post('https://api.twitter.com/oauth/access_token', null, {
-            params: {
-                grant_type: 'authorization_code',
-                client_id: 'V1FrUFdVZ3picVFSUGtHWExpR1I6MTpjaQ',
-                client_secret: 'goM30d8fWPtizfTnohJhwIkDy1daOYJfiuc7M-tnpcO_PjOdLh',
-                code,
-                redirect_uri: 'http://localhost:5173/twitter-success', // Must match the original redirect URI
-            },
-        });
-        console.log(response);
-        const accessToken = response.data.access_token;
-        // Now you have the access token!
+// watch(code, async () => {
+//     if (code) {
+//         const response = await axios.post('https://api.twitter.com/oauth/access_token', null, {
+//             params: {
+//                 grant_type: 'authorization_code',
+//                 client_id: 'V1FrUFdVZ3picVFSUGtHWExpR1I6MTpjaQ',
+//                 client_secret: 'goM30d8fWPtizfTnohJhwIkDy1daOYJfiuc7M-tnpcO_PjOdLh',
+//                 code,
+//                 redirect_uri: 'https://tally-test.onrender.com/twitter-success', // Must match the original redirect URI
+//             },
+//         });
+//         console.log(response);
+//         const accessToken = response.data.access_token;
+//         // Now you have the access token!
 
-        // Use the access token to make authenticated requests to Twitter API
-        // For example, get user data:
-        const userResponse = await axios.get('https://api.twitter.com/2/users/me', {
-            headers: {
-                Authorization: `Bearer ${accessToken}`,
-            },
-        });
-        console.log(userResponse);
-        const userId = userResponse.data.id;
-        const userName = userResponse.data.username;
-        // Now you have the user data!
-        console.log(userName);
-        // Handle the user data as needed (e.g., store in database, etc.)
+//         // Use the access token to make authenticated requests to Twitter API
+//         // For example, get user data:
+//         const userResponse = await axios.get('https://api.twitter.com/2/users/me', {
+//             headers: {
+//                 Authorization: `Bearer ${accessToken}`,
+//             },
+//         });
+//         console.log(userResponse);
+//         const userId = userResponse.data.id;
+//         const userName = userResponse.data.username;
+//         // Now you have the user data!
+//         console.log(userName);
+//         // Handle the user data as needed (e.g., store in database, etc.)
 
-        // Redirect the user to a success page
-        res.redirect('/success');
-    }
-}, { deep: true })
+//         // Redirect the user to a success page
+//         res.redirect('/success');
+//     }
+// }, { deep: true })
 
 
 onMounted(async () => {
